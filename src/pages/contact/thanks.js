720f62718aa6a4d8b35a2d44c1ba74e90e772bc1
meganrm <meganrm@gmail.com>
2020-05-13 23:10:16 -0700

    updating thanks copy

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -7,7 +7,7 @@ export default () => (
       <div className="container">
         <div className="content">
           <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <p>We will take a look at your submission as soon as possible.</p>
         </div>
       </div>
     </section>
