0572d31e756edf82063acfd1db4a7164a6943af4
meganrm <meganrm@gmail.com>
2020-05-17 16:26:51 -0700

    adding link to facebook post

--- src/components/FacebookEmbed.js
+++ src/components/FacebookEmbed.js
@@ -7,7 +7,8 @@ export const FacebookEmbed = ({ link, slug }) => {
   const src = `https://www.facebook.com/plugins/post.php?href=${formattedUrl}`
   
   return (
-    <div className={slug}>
+    <div className={`${slug} facebook-post`}>
+      <a href={link} target="_blank"></a>
       <iframe title={slug} src={src} width="100%" height="440" style={{border:'none', overflow:'hidden'}} scrolling="no" frameBorder="0" allowTransparency={true} allow="encrypted-media"></iframe>
     </div>
   
