4f6df70e8c7886709ba97c0d88f135e4ff737d09
meganrm <meganrm@gmail.com>
2020-05-25 14:55:33 -0700

    removing colcade

--- gatsby-node.js
+++ gatsby-node.js
@@ -111,19 +111,3 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   }
 }
 
-exports.onCreateWebpackConfig = ({
-  stage,
-  loaders,
-  actions
-}) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [{
-          test: /colcade/,
-          use: loaders.null(),
-        }, ],
-      },
-    })
-  }
-}
