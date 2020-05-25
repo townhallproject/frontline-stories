ec91016ee60e0e2715dfc1557f6041a26d348ee9
meganrm <meganrm@gmail.com>
2020-05-24 19:57:24 -0700

    clean up tags and states

--- src/templates/states.js
+++ src/templates/states.js
@@ -1,46 +1,13 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { Link, graphql } from 'gatsby'
-import Colcade from 'colcade';
 
 import Layout from '../components/Layout'
-import Post from '../components/Post'
-import {
-  calcGridSize
-} from '../utils';
+
 import GridView from '../components/GridView';
 
 class StatesRoute extends React.Component {
-  handleMount = (id) => {
-    const {
-      data
-    } = this.props;
 
-    const {
-      edges: posts
-    } = data.allMarkdownRemark;
-
-    if (id === posts.length - 1) {
-      var grid = document.querySelector('.story-wall');
-
-      this.colc = new Colcade(grid, {
-        columns: '.grid-col',
-        items: '.card-container'
-      });
-    }
-  }
-  onSize = (size, id) => {
-    calcGridSize('.story-wall', '.story-wall .card-container');
-  }
-
-  handleEnter = (event, id) => {
-    if (!this.state[id]) {
-
-      this.setState({
-        [id]: true
-      })
-    }
-  }
   render() {
     const posts = this.props.data.allMarkdownRemark.edges
     const state = this.props.pageContext.state
@@ -62,7 +29,9 @@ class StatesRoute extends React.Component {
               >
                 <h3 className="title is-size-4 is-bold-light">{stateHeader}</h3>
                 <GridView posts={posts} />
+                <p>
                   <Link to="/states/">Browse all states</Link>
+                </p>
               </div>
             </div>
           </div>
--- src/templates/tags.js
+++ src/templates/tags.js
@@ -2,17 +2,11 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import GridView from '../components/GridView';
 
 class TagRoute extends React.Component {
   render() {
     const posts = this.props.data.allMarkdownRemark.edges
-    const postLinks = posts.map(post => (
-      <li key={post.node.fields.slug}>
-        <Link to={post.node.fields.slug}>
-          <h2 className="is-size-2">{post.node.frontmatter.name}</h2>
-        </Link>
-      </li>
-    ))
     const tag = this.props.pageContext.tag
     const title = this.props.data.site.siteMetadata.title
     const totalCount = this.props.data.allMarkdownRemark.totalCount
@@ -31,7 +25,7 @@ class TagRoute extends React.Component {
                 style={{ marginBottom: '6rem' }}
               >
                 <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
-                <ul className="taglist">{postLinks}</ul>
+                <GridView posts={posts} />
                 <p>
                   <Link to="/tags/">Browse all tags</Link>
                 </p>
@@ -66,6 +60,11 @@ export const tagPageQuery = graphql`
           }
           frontmatter {
             name
+            name
+            source
+            link
+            tags
+            state
           }
         }
       }
